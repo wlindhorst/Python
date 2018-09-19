f456a0d00f255f1a3c0cd851349f7273d5add6f3
wlindhorst <wlindhorst@gmail.com>
2018-09-19 13:30:27 -0500

    Initial web scrape fn

--- Chapter1/scrapetest.py
+++ Chapter1/scrapetest.py
@@ -1,6 +1,21 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bs = BeautifulSoup(html.read(), 'html.parser')
+        title = bs.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle('http://pythonscraping.com/pages/page1.html')
+if title==None:
+    print('Title could not be found')
+else:
+    print(title)
